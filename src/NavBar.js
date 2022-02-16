b9c65409ebca32cf558fcd020210628fcc9851d6
Yoan Kirilov <p110133523@student.peterborough.ac.uk>
2022-02-16 15:50:29 +0000

    YEp2

--- src/NavBar.js
+++ src/NavBar.js
@@ -14,28 +14,26 @@ const Navbar  = (props) => {
           </div>
 
           <div className="Menu">
-               <div class="row g-3">
-                    <div className="links"> 
-                         <Stack direction="horizontal" gap={5}> 
-                              <Link to="#">Products</Link>
-                              <Link to="#">Purchase Orders</Link>
-                              <Link to="#">Staff</Link>
-                              <Link to="#">Report</Link>
-                              <Link to="#">Help/Contact us</Link>
-                              <form action="/" method="get">
-                                   <label htmlFor="header-search">
-                                        <span className="visually-hidden">Keyword, Product....</span>
-                                   </label>
-                                   <input
-                                        type="text"
-                                        id="header-search"
-                                        placeholder="Keyword, Product...."
-                                        name="s" 
-                                   />
-                                   <button type="submit">Search</button>
-                              </form>
-                         </Stack>
-                    </div>
+               <div className="links"> 
+                    <Stack direction="horizontal" gap={5}> 
+                         <Link to="#">Products</Link>
+                         <Link to="#">Purchase Orders</Link>
+                         <Link to="#">Staff</Link>
+                         <Link to="#">Report</Link>
+                         <Link to="#">Help/Contact us</Link>
+                         <form action="/" method="get">
+                              <label htmlFor="header-search">
+                                   <span className="visually-hidden">Keyword, Product....</span>
+                              </label>
+                              <input
+                                   type="text"
+                                   id="header-search"  
+                                   placeholder="Keyword, Product...."
+                                   name="s" 
+                              />
+                              <button type="submit">Search</button>
+                         </form>
+                    </Stack>
                </div>
           </div>
           </nav>
