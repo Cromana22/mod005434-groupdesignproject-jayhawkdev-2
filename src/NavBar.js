b656c611dadda00cefe32399d788ebd7a39d7132
Yoan Kirilov <p110133523@student.peterborough.ac.uk>
2022-02-16 15:05:50 +0000

    More stuff to our navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,31 @@
 import './NavBar.css';
 import { Link } from "react-router-dom";
 import Logo from './Logo.png';
-import Container  from 'react-bootstrap';
+import Stack from 'react-bootstrap/Stack'
 
 const Navbar  = () => {
     return ( 
-     <div class="container">
+         <nav className="menu">
           <div className="navbar">
                <img id="logo" src={Logo}></img>
                <h1>Products</h1>
                <button>Logout</button> 
           </div>
 
-          <div className="products">
+          <div className="Menu">
                <div class="row g-3">
-               <h1>Products</h1>
+                    <div className="links"> 
+                         <Stack direction="horizontal" gap={5}> 
+                              <Link to="#">Products</Link>
+                              <Link to="#">Purchase Orders</Link>
+                              <Link to="#">Staff</Link>
+                              <Link to="#">Report</Link>
+                              <Link to="#">Help/Contact us</Link>
+                         </Stack>
+                    </div>
                </div>
           </div>
-      </div>
+          </nav>
      );
 }
  
