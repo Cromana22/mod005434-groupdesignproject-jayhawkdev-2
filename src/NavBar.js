01df66155280639059ec68205c4953a2f13aa5d2
Yoan Kirilov <p110133523@student.peterborough.ac.uk>
2022-02-22 16:05:56 +0000

    New navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,17 @@
 import './NavBar.css';
 import { Link } from "react-router-dom";
 import Logo from './Logo.png';
-import Stack from 'react-bootstrap/Stack';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
-const Navbar  = (props) => {
+
+
+const navbar  = (props) => {
      const {title} = props
     return ( 
          <nav className="menu">
@@ -14,30 +20,46 @@ const Navbar  = (props) => {
                <h1>{title}</h1>
                <Link to='/login'><button>Logout</button></Link> 
           </div>
-          <Row>
-               <Col><Link to="#">Products</Link></Col>
-               <Col><Link to="#">Purchase Orders</Link> </Col>
-               <Col><Link to="#">Staff</Link></Col>
-               <Col><Link to="#">Report</Link> </Col>
-               <Col><Link to="#">Help/Contact us</Link></Col>
-               <Col xs={6}> 
-                    <form className="form" action="/" method="get">
-                              <label htmlFor="header-search">
-                                   <span className="visually-hidden">Keyword, Product....</span>
-                              </label>
-                              <input
-                                   type="text"
-                                   id="header-search"  
-                                   placeholder="Products, Basket, PO...."
-                                   name="s" 
-                              />
-                              <button type="submit">Search</button>
-                    </form>
-               </Col>
-          </Row>
-          
+          <div className='barmenu'>
+          <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+        <NavDropdown title="Link" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+          </div>
      </nav>
      );
 }
  
-export default Navbar;
+export default navbar;
