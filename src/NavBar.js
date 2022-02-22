38ff544fcc7d9e3d46e8a15ee29e5b86a0c7b38d
Hussnain Zafar <husnain.zafar2626@gmail.com>
2022-02-22 16:38:57 +0000

    CSS customisation of bootstrap Navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -23,7 +23,6 @@ const navbar  = (props) => {
           <div className='barmenu'>
           <Navbar bg="light" expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -31,24 +30,16 @@ const navbar  = (props) => {
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
+        <Nav.Link href="#action1">Products</Nav.Link>
+        <Nav.Link href="#action2">Purchase Orders</Nav.Link>
+        <Nav.Link href="#action2">Staff</Nav.Link>
+        <Nav.Link href="#action2">Report</Nav.Link>
+        <Nav.Link href="#action2">Help/Contact Us</Nav.Link>
       </Nav>
       <Form className="d-flex">
         <FormControl
           type="search"
-          placeholder="Search"
+          placeholder="Products, Basket, PO..."
           className="me-2"
           aria-label="Search"
         />
