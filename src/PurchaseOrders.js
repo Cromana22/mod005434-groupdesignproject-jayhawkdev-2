3d73e05f725459f0c4a66f659032a221b3056e8d
Yoan Kirilov <p110133523@student.peterborough.ac.uk>
2022-02-22 16:44:05 +0000

    The data in our table are set

--- src/PurchaseOrders.js
+++ src/PurchaseOrders.js
@@ -1,23 +1,23 @@
 import './PurchaseOrders.css';
 import * as React from 'react';
-import {DataGrid} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'PO Number', type: 'number', width: 120 },
-  { field: 'productName', headerName: 'Raised By', type: 'text', width: 150 },
-  { field: 'qtyAvailable', headerName: 'Raised Date', type: 'date', width: 130 },
-  { field: 'age', headerName: 'Supplier', type: 'text', width: 130},
-  { field: 'stockstatus', headerName: 'Total Value', width: 150},
-  { field: 'qtytoorder', headerName: 'Status', width: 150 },
+  { field: 'id', headerName: 'PO Number', type: 'number', width: 110 },
+  { field: 'raisedby', headerName: 'Raised By', width: 150 },
+  { field: 'raiseddate', headerName: 'Raised Date', width: 150 },
+  { field: 'supplier', headerName: 'Supplier', width: 130 },
+  { field: 5, headerName: 'Total Value', width: 100 },
+  { field: 'status', headerName: 'Status', width: 110 },
 ];
 
 const rows = [
-  { id: 1, productName: 'Nerf N-Strike', qtyAvailable: 50, age: 35 },
-  { id: 2, productName: 'LEGO Classic bricks', qtyAvailable: 50, age: 42 },
-  { id: 3, productName: 'Polaroid Play 3D Pen', qtyAvailable: 50, age: 45 },
-  { id: 4, productName: 'USB Powerbank', qtyAvailable: 50, age: 16 },
-  { id: 5, productName: 'Spider catcher', qtyAvailable: 50, age: 20 },
-  { id: 6, productName: 'Star Wars USB Cup', qtyAvailable: 50, age: 150 },
+  { id: '#QB123456', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 10.67, status: 'Authorised'},
+  { id: '#AC789006', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 690.20, status: 'Authorised'},
+  { id: '#TY122156', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 50.80, status: 'Unauthorised'},
+  { id: '#QB857390', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 9.99, status: 'Authorised'},
+  { id: '#GH159753', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 27.00, status: 'Unauthorised'},
+  { id: '#NJ3578464', raisedby: 'HussnainZafar', raiseddate: '20/01/2022  10:38AM', supplier: 'Bitmore Inc.', 5: 87.12, status: 'Unauthorised'},
 ];
 
 export default function DataTable() {
