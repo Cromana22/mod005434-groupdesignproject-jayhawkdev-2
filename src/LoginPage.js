4538105110441962277a101dabca3a6ccf30c054
Hussnain <p110136519@student.peterborough.ac.uk>
2022-02-23 16:18:01 +0000

    login page css

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -6,24 +6,32 @@ import Logo from './Logo.png';
 const LoginPage  = () => {
     return (
       <div className='LoginPage'>
-      <img id="logo" src={Logo}/>
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="username" placeholder="Enter username" />
-          <Form.Text className="text-muted">
-          </Form.Text>
-        </Form.Group>
-      
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        <Button className="login" variant="primary" type="submit" href="/products">Log in</Button>
-      </Form>
+        <div className='loginform'>
+          <img id="logo" src={Logo}/>
+          <Form>
+
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Enter username" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+          
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+
+            <div id="loginbtn">
+              <Button className="login" variant="primary" type="submit" href="/products">Log In</Button>
+            </div>
+
+          </Form>
+        </div>
       </div>
     );
 }
