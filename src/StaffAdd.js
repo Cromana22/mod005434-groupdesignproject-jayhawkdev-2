769b73ba3f44d221926eae1a668cc125e6c93638
P110136416 <p110136416@student.peterborough.ac.uk>
2022-03-17 13:25:10 +0000

    add staff functional (single product type)

--- src/StaffAdd.js
+++ src/StaffAdd.js
@@ -9,17 +9,17 @@ const StaffAdd = (props) => {
       <NavBar title='Add New Staff' basketCount={basketCount} />
       <div className="AddStaffForm">
         <form method="POST" action={phpUrl+"/staffadd.php"}>
-          <label>Staff ID: </label><input type='text' name='staffId'></input><br />
-          <label>Title: </label><input type='text' name='title'></input><br />
-          <label>First Name: </label><input type='text' name='firstName'></input><br />
-          <label>Surname: </label><input type='text' name='surname'></input><br />
-          <label>Email: </label><input type='text' name='email'></input><br />
-          <label>Job Title: </label><input type='text' name='jobTitle'></input><br />
-          <label>Shop: </label><input type='text' name='shopId'></input><br />
-          <label>Department: </label><input type='text' name='deptId'></input><br />
+          <label>Staff ID: </label><input type='text' name='staffId' required></input><br />
+          <label>Title: </label><input type='text' name='title' required></input><br />
+          <label>First Name: </label><input type='text' name='firstName' required></input><br />
+          <label>Surname: </label><input type='text' name='surname' required></input><br />
+          <label>Email: </label><input type='text' name='email' required></input><br />
+          <label>Job Title: </label><input type='text' name='jobTitle' required></input><br />
+          <label>Shop: </label><input type='text' name='shopName'></input><br />
+          <label>Department: </label><input type='text' name='deptName'></input><br />
           <label>Product Responsibilities: </label><input type='text' name='productTypes'></input><br />
-          <label>Access Level: </label><input type='text' name='accessLevel'></input><br />
-          <label>Password: </label><input type='text' name='password'></input><br />
+          <label>Access Level: </label><input type='text' name='accessLevel' required></input><br />
+          <label>Password: </label><input type='text' name='password' required></input><br />
           <button type='submit'>Submit</button>
         </form>
       </div>
--- src/php/staffadd.php
+++ src/php/staffadd.php
@@ -6,12 +6,46 @@ header("Access-Control-Allow-Origin: $webUrl");
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Credentials: true');
 
-if (isset($_POST['staffId'])) { $staffId = $_POST['staffId']; } else { $staffId = "error"; };
+if (isset($_POST['staffId'])) { $staffId = $_POST['staffId']; } else { $staffId = "deleteme"; };
+if (isset($_POST['title'])) { $title = $_POST['title']; } else { $title = "deleteme"; };
+if (isset($_POST['firstName'])) { $firstName = $_POST['firstName']; } else { $firstName = "deleteme"; };
+if (isset($_POST['surname'])) { $surname = $_POST['surname']; } else { $surname = "deleteme"; };
+if (isset($_POST['email'])) { $email = $_POST['email']; } else { $email = "deleteme"; };
+if (isset($_POST['jobTitle'])) { $jobTitle = $_POST['jobTitle']; } else { $jobTitle = "deleteme"; };
+if (isset($_POST['shopName'])) { $shopName = $_POST['shopName']; } else { $shopName = "null"; };
+if (isset($_POST['deptName'])) { $deptName = $_POST['deptName']; } else { $deptName = "null"; };
+if (isset($_POST['accessLevel'])) { $accessLevel = $_POST['accessLevel']; } else { $accessLevel = "deleteme"; };
+if (isset($_POST['password'])) { $password = $_POST['password']; } else { $password = "deleteme"; };
+if ($accessLevel == "Sales" || $accessLevel == "Senior Sales") { $homepage = "products"; } else { $homepage = "purchaseorders"; };
 
-$insert = $pdo->prepare("INSERT INTO PurchaseOrder (staffId) VALUES (:staffId)");
-$insert->bindParam(':staffId', $staffId, PDO::PARAM_STR);
-$insert->execute();
+$insert1 = $pdo->prepare("INSERT INTO `Staff` (`staffId`, `title`, `firstName`, `surname`, `email`, `jobTitle`, `password`, `shopId`, `deptId`, `accessLevel`, `homepage`)
+VALUES (:staffId, :title, :firstName, :surname, :email, :jobTitle, :passwrd, :shopId, :deptId, :accessLevel, :homepage)");
+$insert1->bindParam(':staffId', $staffId, PDO::PARAM_STR);
+$insert1->bindParam(':title', $title, PDO::PARAM_STR);
+$insert1->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+$insert1->bindParam(':surname', $surname, PDO::PARAM_STR);
+$insert1->bindParam(':email', $email, PDO::PARAM_STR);
+$insert1->bindParam(':jobTitle', $jobTitle, PDO::PARAM_STR);
+$insert1->bindParam(':passwrd', $password, PDO::PARAM_STR);
+$insert1->bindParam(':shopId', $shopId, PDO::PARAM_STR);
+$insert1->bindParam(':deptId', $deptId, PDO::PARAM_STR);
+$insert1->bindParam(':accessLevel', $accessLevel, PDO::PARAM_STR);
+$insert1->bindParam(':homepage', $homepage, PDO::PARAM_STR);
+$insert1->execute();
 
-echo "<script>location.replace('$webUrl/purchaseorders')</script>";
+if (isset($_POST['productTypes'])) { 
+    $productTypes = $_POST['productTypes'];
+
+    //foreach ($productTypes as $productType) {
+        $productTypes = substr($productTypes, 0, 1);
+        $insert2 = $pdo->prepare("INSERT INTO `StaffProductType` (`staffId`, `productType`) VALUES (:staffId, :productType)");
+        $insert2->bindParam(':staffId', $staffId, PDO::PARAM_STR);
+        $insert2->bindParam(':productType', $productTypes, PDO::PARAM_STR);
+        $insert2->execute();
+    //}
+};
+
+
+echo "<script>alert('Staff Successfully Created'); location.replace('$webUrl/staff')</script>";
 ?>
 
